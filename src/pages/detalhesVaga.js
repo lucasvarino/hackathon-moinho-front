c94b5aec81d84e1acb4ed083d6f211d6d83ad1ca
Gabriel Frasson <bielfc27@gmail.com>
2023-01-21 16:06:41 -0300

    inicio pagina detalhes vaga

---
+++ src/pages/detalhesVaga.js
@@ -0,0 +1,25 @@
+// import {  } from "@/components/Header";
+import Logo from "@/../public/images/logo-albert-sabin.png";
+
+// import styles from "@/styles/offerDetails.css";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    // <Header></Header>
+
+    <div>
+        <div>
+            <h1 className="text-light-green">asdasdasd</h1>
+            <Image src={Logo} alt="Logo da instituição" />
+        </div>
+
+        
+        <main>
+
+        </main>
+
+    </div>
+
+  );
+}
