585a6342dda206e67444bd36a9c6b73a90b45cdc
Gabriel Frasson <bielfc27@gmail.com>
2023-01-21 16:08:42 -0300

    tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: [ // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#2C2C2C",
+        primary: "#EBEBEB",
+        secondary: "#D9D9D9",
+        teal: "#1CA4A4",
+        "light-green": "#7FEF7D",
+        "dark-green": "#216416",
+        "light-red": "#EF7D7D",
+        "dark-red": "#611515",
+      }
+    },
   },
   plugins: [],
 }
