8d87b57aa419d927c2801eae3cf4a62201c025a8
Lucas Varino <lucasvarino@gmail.com>
2023-01-21 19:54:56 -0300

    Card Pronto

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,40 @@
+import Image from "next/image";
+import { MapPinIcon } from "@heroicons/react/24/outline";
+
 export default function Card() {
   return (
     <>
       <div class="max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow">
-        <a href="#">
+        <a href="#" className="flex items-center justify-around">
+          <Image src="hospital.svg" alt="Hospital" width={100} height={100} />
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-            Noteworthy technology acquisitions 2021
+            Hospital Albert Sabin
           </h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 ">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+        <a href="#">
+          <h6 className="text-xl">Ortopedista Pediatra</h6>
+        </a>
+        <div className="flex justify-between">
+          <p className="text-gray-700 font-bold text-xs my-3">1 dia atrás</p>
+          <p className="text-gray-700 font-bold text-xs my-3">Integral</p>
+          <p className="text-gray-700 font-bold text-xs my-3">R$50.000</p>
+          <p className="text-gray-700 font-bold text-xs my-3">
+            45 pessoas aplicaram
+          </p>
+        </div>
+        <p class="mb-3 font-normal text-gray-700 text-sm">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic
         </p>
+        <div className="flex justify-start">
+          <MapPinIcon height={17} width={17} className="mr-3" />
+          <p className="mb-3 text-gray-700 font-bold text-sm">
+            Avenida Rio Branco, 95, Centro
+          </p>
+        </div>
         <a
           href="#"
           class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 "
