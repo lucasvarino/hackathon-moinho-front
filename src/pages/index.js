760c151b8585dfed3a4533eb0295f8286264719d
Lucas Varino <lucasvarino@gmail.com>
2023-01-21 16:05:14 -0300

    Navbar completa

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <h1>Teste</h1>
+      </div>
+    </>
+  );
 }
