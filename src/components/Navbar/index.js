8df7e33ea88d4fe272b737312af5446f834607ec
Lucas Varino <lucasvarino@gmail.com>
2023-01-22 00:35:20 -0300

    Correções

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -75,7 +75,7 @@ export default function Navbar() {
               Entrar
             </Link>
             <Link
-              href="/cadastrar"
+              href="/cadastroUsuario"
               className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
             >
               Cadastrar
--- src/pages/cadastroUsuario.js
+++ src/pages/cadastroUsuario.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/footer";
 import Navbar from "@/components/Navbar";
 import { useState } from "react";
 
@@ -12,16 +13,16 @@ export default function CadastroUsuario() {
   const userTypes = [
     {
       value: "0",
-      name: "Instituição"
+      name: "Instituição",
     },
     {
       value: "1",
-      name: "Profissional"
+      name: "Profissional",
     },
     {
       value: "2",
-      name: "Professor"
-    }
+      name: "Professor",
+    },
   ];
 
   return (
@@ -37,7 +38,10 @@ export default function CadastroUsuario() {
                 <div className="bg-white px-4 py-5 sm:p-6">
                   <div className="grid grid-cols-6 gap-6">
                     <div className="col-span-6 sm:col-span-6">
-                      <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                      <label
+                        htmlFor="name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
                         Nome
                       </label>
                       <input
@@ -52,7 +56,10 @@ export default function CadastroUsuario() {
                     </div>
 
                     <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                      <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-700"
+                      >
                         Email
                       </label>
                       <input
@@ -67,7 +74,10 @@ export default function CadastroUsuario() {
                     </div>
 
                     <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                      <label
+                        htmlFor="password"
+                        className="block text-sm font-medium text-gray-700"
+                      >
                         Senha
                       </label>
                       <input
@@ -80,7 +90,10 @@ export default function CadastroUsuario() {
                       />
                     </div>
                     <div className="col-span-6 sm:col-span-6">
-                      <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+                      <label
+                        htmlFor="description"
+                        className="block text-sm font-medium text-gray-700"
+                      >
                         Descrição
                       </label>
                       <textarea
@@ -93,7 +106,10 @@ export default function CadastroUsuario() {
                     </div>
 
                     <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="user_type" className="block text-sm font-medium text-gray-700">
+                      <label
+                        htmlFor="user_type"
+                        className="block text-sm font-medium text-gray-700"
+                      >
                         Tipo de usuário
                       </label>
                       <select
@@ -101,23 +117,28 @@ export default function CadastroUsuario() {
                         name="user_type"
                         autoComplete="user_type"
                         className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        onChange={(event) => setUserTypeSelected(event.target.value)}
-                        defaultValue={userTypes.find((userType) => userType.name === "Profissional").value}
-                      >
-                        {
-                          userTypes.map(userType => (
-                            <option
-                              key={userType.value}
-                              value={userType.value}
-                            >{userType.name}</option>
-                          ))
+                        onChange={(event) =>
+                          setUserTypeSelected(event.target.value)
                         }
+                        defaultValue={
+                          userTypes.find(
+                            (userType) => userType.name === "Profissional"
+                          ).value
+                        }
+                      >
+                        {userTypes.map((userType) => (
+                          <option key={userType.value} value={userType.value}>
+                            {userType.name}
+                          </option>
+                        ))}
                       </select>
                     </div>
-                    {
-                      userTypeSelected === "0" &&
+                    {userTypeSelected === "0" && (
                       <div className="col-span-6 sm:col-span-3">
-                        <label htmlFor="cnpj" className="block text-sm font-medium text-gray-700">
+                        <label
+                          htmlFor="cnpj"
+                          className="block text-sm font-medium text-gray-700"
+                        >
                           CNPJ
                         </label>
                         <input
@@ -130,7 +151,7 @@ export default function CadastroUsuario() {
                           value={cnpj}
                         />
                       </div>
-                    }
+                    )}
                   </div>
                 </div>
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
@@ -146,6 +167,7 @@ export default function CadastroUsuario() {
           </div>
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
